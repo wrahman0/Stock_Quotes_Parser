4a77cc11114b4fa729ab788404c24707fbc6aae8
Wasiur Rahman <wrahman0@gmail.com>
2014-05-10 20:12:30 -0400

    More Dev

--- gen/com/example/stockquotes/R.java
+++ gen/com/example/stockquotes/R.java
@@ -31,19 +31,22 @@ public final class R {
     }
     public static final class id {
         public static final int TableLayout1=0x7f090000;
-        public static final int action_settings=0x7f09000a;
+        public static final int action_settings=0x7f09000c;
         public static final int deleteStocksButton=0x7f090009;
         public static final int enterStockSymbolButton=0x7f090004;
         public static final int enterSymbolTextView=0x7f090002;
-        public static final int stockScrollView=0x7f090008;
+        public static final int quoteFromWebButton=0x7f09000b;
+        public static final int stockQuoteButton=0x7f09000a;
         public static final int stockSymbolEditText=0x7f090003;
         public static final int stockSymbolTextView=0x7f090006;
+        public static final int stockTableScrollView=0x7f090008;
         public static final int tableRow1=0x7f090001;
         public static final int tableRow2=0x7f090005;
         public static final int tableRow3=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int stock_quote_row=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/example/stockquotes/MainActivity.java
+++ src/com/example/stockquotes/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.stockquotes;
 
+import java.util.Arrays;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
 
 public class MainActivity extends Activity {
 
+	
+	public final static String STOCK_SYMBOL = "com.example.stockquote.STOCK";
+	private SharedPreferences stockSymbolsEntered;
+	private TableLayout stockTableScrollView;
+	private EditText stockSymbolEditText;
+	
+	Button enterStockSymbolButton;
+	Button deleteStocksButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Retrieve Saved Stocks
+		stockSymbolsEntered = getSharedPreferences("stockList", MODE_PRIVATE);
+		
+		//Setup the rest of the variables
+		stockTableScrollView = (TableLayout) findViewById(R.id.stockTableScrollView);
+		stockSymbolEditText = (EditText) findViewById (R.id.stockSymbolEditText);
+		enterStockSymbolButton = (Button) findViewById (R.id.enterStockSymbolButton);
+		deleteStocksButton = (Button) findViewById (R.id.deleteStocksButton);
+		
+		//Click Listeners to the buttons
+		enterStockSymbolButton.setOnClickListener(enterStockButtonListener);
+		deleteStocksButton.setOnClickListener(deleteStocksButtonListener);
+		
+		//Add saved stocks to the ScrollView
+		updateSavedStockList(null);
+		
+		
+	}
+	
+	//Add new stock or update with saved stocks if null is passed through
+	private void updateSavedStockList(String symbolToAdd){
+		
+		//Get the saved stocks
+		String[] stocks = stockSymbolsEntered.getAll().keySet().toArray(new String[0]);
+		Arrays.sort(stocks, String.CASE_INSENSITIVE_ORDER);
+		
+		//update
+		if (symbolToAdd == null){
+			
+		}
 		
 	}
 
