c52f60d0ff177a25243831f9f4fd3e01d4da5f59
Wasiur Rahman <wrahman0@gmail.com>
2014-06-03 00:11:44 -0400

    More Dev

--- src/com/example/stockquotes/StockInfoActivity.java
+++ src/com/example/stockquotes/StockInfoActivity.java
@@ -1,8 +1,11 @@
 package com.example.stockquotes;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.TextView;
 
-public class StockInfoActivity {
+public class StockInfoActivity extends Activity {
 	
 	private static final String TAG = "STOCKQUOTE";
 	
@@ -12,7 +15,7 @@ public class StockInfoActivity {
 	TextView daysLowTextView;
 	TextView daysHighTextView;
 	TextView lastTradePriceOnlyTextView;
-	TextView changeTextTextView;
+	TextView changeTextView;
 	TextView daysRangeTextView;
 	
 	//Parent node for the xml file
@@ -37,4 +40,33 @@ public class StockInfoActivity {
 	String lastTradePriceOnly = "";
 	String change = "";
 	String daysRange = "";
+	
+	String yahooURLFirst = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22";
+	String yahooURLSecond = "%22)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	
+	protected void OnCreate (Bundle savedInstanceState){
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView (R.layout.activity_stock_info);
+		
+		//Getting the passed info from other activity
+		Intent intent = getIntent();
+		String stockSymbol = intent.getStringExtra(MainActivity.STOCK_SYMBOL);
+		
+		// Initialize TextViews
+		companyNameTextView = (TextView) findViewById(R.id.companyNameTextView);
+		yearLowTextView = (TextView) findViewById(R.id.yearLowTextView);
+		yearHighTextView = (TextView) findViewById(R.id.yearHighTextView);
+		daysLowTextView = (TextView) findViewById(R.id.daysLowTextView);
+		daysHighTextView = (TextView) findViewById(R.id.daysHighTextView);
+		lastTradePriceOnlyTextView = (TextView) findViewById(R.id.lastTradePriceOnlyTextView);
+		changeTextView = (TextView) findViewById(R.id.changeTextView);
+		daysRangeTextView = (TextView) findViewById(R.id.daysRangeTextView);
+		
+		final String yqlURL = yahooURLFirst + stockSymbol + yahooURLSecond;
+		
+		
+	}
+	
 }
